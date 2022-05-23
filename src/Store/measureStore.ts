c9bc0c049b2a1e4d422407ce7f1fcc44820c4eaf
mcmcphillips <matt.mcphillips@semanticbits.com>
2022-05-23 13:42:51 -0700

    Update types, babel config, measureStore, and export file

--- src/Store/measureStore.ts
+++ src/Store/measureStore.ts
@@ -1,28 +1,23 @@
-import React from 'react';
-import { Subject } from 'rxjs';
-import { Measure } from '@madie/madie-models/dist/Measure';
+import React from "react";
+import { Subject } from "rxjs";
+import { Measure } from "@madie/madie-models/dist/Measure";
 
 // immutable object that retains state, tracks updates
 const subject = new Subject<Measure | null>();
 // initial value just for initialization.
-const initialState : null = null;
+const initialState: null = null;
 // this will be the references that updates events are emitted against.
-let state : Measure | null = initialState;
+let state: Measure | null = initialState;
 // expose everything.
 export const measureStore = {
-    // setMeasureState can be swapped with hook setters to auto do update (listen)
-    subscribe: (
-        setMeasureState: React.Dispatch<
-        React.SetStateAction<Measure>>
-    ) =>
-    subject.subscribe((state) => 
-    setMeasureState(state)
-    ),
-    // updateMeasure is mapped to an updating state function
-    updateMeasure: (measure : Measure | null) => {
-        state = Object.assign({}, measure);
-        subject.next(state);
-    },
-    initialState,
-    state
+  // setMeasureState can be swapped with hook setters to auto do update (listen)
+  subscribe: (setMeasureState: React.Dispatch<React.SetStateAction<Measure>>) =>
+    subject.subscribe((state) => setMeasureState(state)),
+  // updateMeasure is mapped to an updating state function
+  updateMeasure: (measure: Measure | null) => {
+    state = Object.assign({}, measure);
+    subject.next(state);
+  },
+  initialState,
+  state,
 };
