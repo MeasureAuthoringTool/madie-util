6131b1d93ddd5d45dc1c57615b18d672009edecf
mcmcphillips <matt.mcphillips@semanticbits.com>
2022-05-26 08:46:26 -0700

    Add entry for serviceConfig

--- src/Config/Config.ts
+++ src/Config/Config.ts
@@ -14,6 +14,9 @@ export interface ServiceConfig {
   elmTranslationService: {
     baseUrl: string;
   };
+  terminologyService: {
+    baseUrl: string;
+  };
 }
 
 export async function getServiceConfig(): Promise<ServiceConfig> {
