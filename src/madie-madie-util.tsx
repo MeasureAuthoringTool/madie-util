cc7c71e2b2d28368350094c6ac4daff951c544eb
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-06-13 17:04:49 -0500

    changes for MAT-4294 refactor

--- src/madie-madie-util.tsx
+++ src/madie-madie-util.tsx
@@ -5,11 +5,12 @@
   if we want other apps to use functions defined here, we need to
     update types file with expected values in each app consuming
 */
-import { getServiceConfig } from "./Config/Config";
+import { getServiceConfig } from "./config/Config";
 import { default as useKeyPress } from "./hooks/useKeyPress";
 import { default as useOktaTokens } from "./hooks/useOktaTokens";
 import { default as useOnClickOutside } from "./hooks/useOnClickOutside";
-import { measureStore } from "./Store/measureStore";
+import { measureStore } from "./store/measureStore";
+import { default as useTerminologyServiceApi } from "./api/useTerminologyServiceApi";
 
 export {
   getServiceConfig,
@@ -17,4 +18,5 @@ export {
   useOktaTokens,
   useOnClickOutside,
   measureStore,
+  useTerminologyServiceApi,
 };
