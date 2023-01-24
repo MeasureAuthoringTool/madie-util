28a43f872cb9dbea000f0853d33d615ea50a66b0
adongare <ashok.dongare@semanticbits.com>
2023-01-24 09:34:05 -0500

    MAT-5248 added feature flag store

---
+++ src/Store/featureFlagStore.ts
@@ -0,0 +1,31 @@
+import React from "react";
+import { BehaviorSubject } from "rxjs";
+
+// immutable object that retains state, tracks updates
+const subject = new BehaviorSubject<FeatureFlags | null>(null);
+interface FeatureFlags {
+  export: boolean;
+  measureVersioning: boolean;
+  populationCriteriaTabs: boolean;
+  applyDefaults: boolean;
+}
+const initialState: FeatureFlags = {
+  export: false,
+  measureVersioning: false,
+  populationCriteriaTabs: false,
+  applyDefaults: false,
+};
+
+let state: FeatureFlags | null = initialState;
+
+export const featureFlagsStore = {
+  subscribe: (
+    setFeatureFlags: React.Dispatch<React.SetStateAction<FeatureFlags>>
+  ) => subject.subscribe((state) => setFeatureFlags(state)),
+  updateFeatureFlags: (featureFlags: FeatureFlags | null) => {
+    state = Object.assign({}, featureFlags);
+    subject.next(state);
+  },
+  initialState,
+  state,
+};
--- src/madie-madie-util.tsx
+++ src/madie-madie-util.tsx
@@ -12,6 +12,7 @@ import { default as useOnClickOutside } from "./hooks/useOnClickOutside";
 import { measureStore } from "./Store/measureStore";
 import { cqlLibraryStore } from "./Store/cqlLibraryStore";
 import { routeHandlerStore } from "./Store/routeHandlerStore";
+import { featureFlagsStore } from "./Store/featureFlagStore";
 import { default as useTerminologyServiceApi } from "./api/useTerminologyServiceApi";
 import { default as useOrganizationApi } from "./api/useOrganizationApi";
 import { useDocumentTitle } from "./hooks/useDocumentTitle";
@@ -25,6 +26,7 @@ export {
   measureStore,
   cqlLibraryStore,
   routeHandlerStore,
+  featureFlagsStore,
   useTerminologyServiceApi,
   useOrganizationApi,
   useDocumentTitle,
