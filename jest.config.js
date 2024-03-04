1a6f5bacb4f15ade1722a807bb336645ae3abbc9
CeciliaLiu8 <cecilialiu8@gmail.com>
2024-03-04 11:57:22 -0600

    fix test error after axios update

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css)$": "identity-obj-proxy",
     "single-spa-react/parcel": "single-spa-react/lib/cjs/parcel.cjs",
+    "^axios$": "axios/dist/node/axios.cjs",
   },
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
