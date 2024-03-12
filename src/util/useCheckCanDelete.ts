d3f2bf02d195ca57a33b48723b04dfc67301377f
Prateek Keerthi <prateekkeerthi7@gmail.com>
2024-03-12 09:28:02 -0500

    MAT-6744 changed variable names

--- src/util/useCheckCanDelete.ts
+++ src/util/useCheckCanDelete.ts
@@ -1,17 +1,17 @@
 import useOktaTokens from "../hooks/useOktaTokens";
 
 const useCheckUserCanDelete = (
-  createdBy: string,
+  user: string,
   draft: boolean = true
 ): boolean => {
   const { getUserName } = useOktaTokens();
-  const userName = getUserName();
+  const loggedInUser = getUserName();
   // versioned measures/libraries can never be deleted.
   if (!draft) {
     return false;
   }
 
-  return createdBy?.toLowerCase() === userName?.toLowerCase();
+  return user?.toLowerCase() === loggedInUser?.toLowerCase();
 };
 
 export default useCheckUserCanDelete;
