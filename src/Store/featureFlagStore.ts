40096538a987370984aa283f06f1ef222087301b
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2024-12-24 16:35:58 -0800

    MAT-7102 add TestCaseListActionCenter flag

--- src/Store/featureFlagStore.ts
+++ src/Store/featureFlagStore.ts
@@ -12,6 +12,7 @@ export interface FeatureFlags {
   enableQdmRepeatTransfer: boolean;
   MeasureButtons: boolean;
   stu6TestCaseValidation: boolean;
+  TestCaseListActionCenter: boolean;
 }
 const initialState: FeatureFlags = {
   CQLBuilderParameters: false,
@@ -22,6 +23,7 @@ const initialState: FeatureFlags = {
   enableQdmRepeatTransfer: false,
   MeasureButtons: false,
   stu6TestCaseValidation: false,
+  TestCaseListActionCenter: false,
 };
 
 let state: FeatureFlags | null = initialState;
