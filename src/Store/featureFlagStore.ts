57e10cc00820d57a6018f780f59a0b41b2b4a26f
Prateek Keerthi <prateekkeerthi7@gmail.com>
2025-02-09 18:15:04 -0500

    MAT-8201 added ShareMeasure feature flag

--- src/Store/featureFlagStore.ts
+++ src/Store/featureFlagStore.ts
@@ -12,6 +12,7 @@ export interface FeatureFlags {
   enableQdmRepeatTransfer: boolean;
   MeasureButtons: boolean;
   stu6TestCaseValidation: boolean;
+  ShareMeasure: boolean;
   TestCaseListActionCenter: boolean;
   QICoreCodeSearch: boolean;
   QICoreValueSetSearch: boolean;
@@ -25,6 +26,7 @@ const initialState: FeatureFlags = {
   enableQdmRepeatTransfer: false,
   MeasureButtons: false,
   stu6TestCaseValidation: false,
+  ShareMeasure: false,
   TestCaseListActionCenter: false,
   QICoreCodeSearch: false,
   QICoreValueSetSearch: false,
