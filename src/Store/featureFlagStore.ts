aed7d3b49a9bb8c9de88523d1589b4aaec54f94d
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2025-09-08 12:57:04 -0700

    MAT-8943 readd EnhancedTextFormatting

--- src/Store/featureFlagStore.ts
+++ src/Store/featureFlagStore.ts
@@ -11,6 +11,7 @@ export interface FeatureFlags {
   MeasureSearch: boolean;
   LibrarySearch: boolean;
   Locking: boolean;
+  EnhancedTextFormatting: boolean;
   qiCore7: boolean;
   TransferMeasure: boolean;
   TransferLibrary: boolean;
@@ -25,6 +26,7 @@ const initialState: FeatureFlags = {
   MeasureSearch: false,
   LibrarySearch: false,
   Locking: false,
+  EnhancedTextFormatting: false,
   qiCore7: false,
   TransferMeasure: false,
   TransferLibrary: false,
