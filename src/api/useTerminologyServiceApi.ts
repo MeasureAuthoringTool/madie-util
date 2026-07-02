6f3a5a61eb8074da77053ca99839a3f367221a1d
mcmcphillips <matt.mcphillips@semanticBits.com>
2026-07-02 09:06:26 -0700

    MAT-8010: add missing method on terminologyService

--- src/api/useTerminologyServiceApi.ts
+++ src/api/useTerminologyServiceApi.ts
@@ -3,8 +3,149 @@ import useOktaTokens from "../hooks/useOktaTokens";
 import { wafIntercept } from "../madie-madie-util";
 import useServiceConfig from "./useServiceConfig";
 
+export type ValueSet = {
+  resourceType: string;
+  id: string;
+  url: string;
+  status: string;
+  errorMsg: string;
+};
+
+export interface ValueSetSearchResult {
+  resultBundle: string;
+  valueSets: ValueSetForSearch[];
+}
+
+export interface ValueSetForSearch {
+  codeSystem?: string;
+  name?: string;
+  author?: string;
+  composedOf?: string;
+  effectiveDate?: string;
+  lastReviewDate?: string;
+  lastUpdated?: string;
+  publisher?: string;
+  purpose?: string;
+  oid?: string;
+  status?: string;
+  steward?: string;
+  title?: string;
+  url?: string;
+  version?: string;
+}
+
 export class TerminologyServiceApi {
   constructor(private baseUrl: string, private getAccessToken: () => string) {}
+
+  async getValueSet(
+    oid: string,
+    locator: string,
+    loggedInUMLS: boolean
+  ): Promise<ValueSet> {
+    let valueset: ValueSet = null;
+    if (!loggedInUMLS) {
+      valueset = {
+        resourceType: "ValueSet",
+        id: oid,
+        url: locator,
+        status: "unauthorized",
+        errorMsg: "Please log in to UMLS",
+      };
+      return valueset;
+    }
+    await axios
+      .get(`${this.baseUrl}/vsac/valueset`, {
+        headers: {
+          Authorization: `Bearer ${this.getAccessToken()}`,
+          "Content-Type": "text/plain",
+        },
+        params: {
+          oid: oid,
+        },
+        timeout: 15000,
+      })
+      .then((resp) => {
+        valueset = resp.data;
+      })
+      .catch((error) => {
+        const message =
+          error.message + " for oid = " + oid + " location = " + locator;
+        valueset = {
+          resourceType: "ValueSet",
+          id: oid,
+          url: locator,
+          status: error.status,
+          errorMsg: message,
+        };
+      });
+    return valueset;
+  }
+
+  async validateCodes(
+    customCqlCodes: any[],
+    loggedInUMLS: boolean,
+    model: string
+  ): Promise<any[]> {
+    if (!loggedInUMLS) {
+      return processCodeSystemErrors(
+        customCqlCodes,
+        "Please Login to UMLS",
+        false
+      );
+    }
+    try {
+      const response = await axios.put(
+        `${this.baseUrl}/vsac/validations/codes?model=${model}`,
+        customCqlCodes,
+        {
+          headers: {
+            Authorization: `Bearer ${this.getAccessToken()}`,
+          },
+        }
+      );
+      if (response.status === 200) {
+        return response.data;
+      } else {
+        return processCodeSystemErrors(
+          customCqlCodes,
+          "Unable to validate code, Please contact HelpDesk",
+          false
+        );
+      }
+    } catch (err) {
+      return processCodeSystemErrors(
+        customCqlCodes,
+        "Unable to validate code, Please contact HelpDesk",
+        false
+      );
+    }
+  }
+  // https://cts.nlm.nih.gov/fhir/ValueSet?usage=VSAC$covid
+  async searchValueSets(values): Promise<ValueSetSearchResult> {
+    const keys = Object.keys(values);
+    let qString = "?";
+    for (let i = 0; i < keys.length; i++) {
+      if (i !== 0) {
+        qString = qString.concat("&");
+      }
+      const key = keys[i];
+      const value = values[key];
+      qString = qString.concat(`${key}=${value}`);
+    }
+    try {
+      const response = await axios.get<ValueSetSearchResult>(
+        `${this.baseUrl}/terminology/search-value-sets${qString}`,
+        {
+          headers: {
+            Authorization: `Bearer ${this.getAccessToken()}`,
+          },
+        }
+      );
+      return response.data;
+    } catch (err) {
+      console.error("Error retrieving getAllCodeSystems: ", err);
+    }
+  }
 
   async checkLogin(): Promise<Boolean> {
     try {
@@ -62,6 +203,27 @@ export class TerminologyServiceApi {
     }
   }
 }
+
+const processCodeSystemErrors = (
+  cqlCodes: any[],
+  errorMessage: string,
+  valid: boolean
+): any[] => {
+  return cqlCodes.map((code) => {
+    return {
+      ...code,
+      errorMessage: errorMessage,
+      valid: valid,
+      ...(code.codeSystem && {
+        codeSystem: {
+          ...code.codeSystem,
+          errorMessage: errorMessage,
+          valid: valid,
+        },
+      }),
+    };
+  });
+};
 
 axios.interceptors.response.use((response) => {
   return response;
