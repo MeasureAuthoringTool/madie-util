8e9f1b5806c5a034b92af419e81447935288dacf
mcmcphillips <matt.mcphillips@semanticBits.com>
2026-09-03 12:13:46 -0700

    MAT-10370: Add delete, update utils

--- src/api/useTerminologyServiceApi.ts
+++ src/api/useTerminologyServiceApi.ts
@@ -4,6 +4,15 @@ import { wafIntercept } from "../madie-madie-util";
 import useServiceConfig from "./useServiceConfig";
 import { CqlCode, CqlCodeSystem } from "@madie/cql-antlr-parser/dist/src";
 
+export interface Page<T> {
+  content: T[];
+  totalElements: number;
+  totalPages: number;
+  number: number;
+  size: number;
+  numberOfElements: number;
+}
+
 // customCqlCode contains validation result from VSAC
 // This object can be cached in future, to avoid calling VSAC everytime.
 export interface CustomCqlCodeSystem extends CqlCodeSystem {
@@ -22,6 +31,35 @@ export type ValueSet = {
   status: string;
   errorMsg: string;
 };
+
+export interface AddValueSetForAdmin {
+  url: string;
+  version?: string;
+  lastUpdated: string;
+  manuallyModified: boolean;
+  valueSet: string;
+}
+export interface ValueSetDisplayForAdmin {
+  id: string;
+  url: string;
+  version?: string;
+  lastUpdated: string;
+  manuallyModified: boolean;
+  valueSet: string;
+}
+
+export interface ValueSetDisplayForAdmin {
+  id: string;
+  url: string;
+  version?: string;
+  lastUpdated: string;
+  manuallyModified: boolean;
+  valueSet: string;
+}
+
+export interface UpdateValueSetForAdmin extends AddValueSetForAdmin {
+  id: string;
+}
 
 export interface ValueSetSearchResult {
   resultBundle: string;
@@ -93,6 +131,37 @@ export class TerminologyServiceApi {
     return valueset;
   }
 
+  async getValueSets(
+    page = 0,
+    limit = 10,
+    sortInfo?: string,
+    searchTerm?: string
+  ): Promise<Page<ValueSetDisplayForAdmin>> {
+    const params: Record<string, string | number> = {
+      page,
+      limit,
+    };
+
+    if (sortInfo) {
+      params.sortInfo = sortInfo;
+    }
+
+    if (searchTerm) {
+      params.searchTerm = searchTerm;
+    }
+
+    const response = await axios.get(
+      `${this.baseUrl}/terminology/admin/valuesets`,
+      {
+        headers: {
+          Authorization: `Bearer ${this.getAccessToken()}`,
+        },
+        params,
+      }
+    );
+
+    return response.data;
+  }
   async validateCodes(
     customCqlCodes: CustomCqlCode[],
     loggedInUMLS: boolean,
@@ -174,6 +243,82 @@ export class TerminologyServiceApi {
       return resp.status === 200;
     } catch (error) {
       throw error;
+    }
+  }
+
+  async deleteValueSet(id: string): Promise<Response> {
+    return await axios.delete(
+      `${this.baseUrl}/terminology/admin/value-set/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${this.getAccessToken()}`,
+        },
+      }
+    );
+  }
+  async updateValueSet(
+    valueSet: UpdateValueSetForAdmin
+  ): Promise<ValueSetDisplayForAdmin> {
+    try {
+      const response = await axios.put(
+        `${this.baseUrl}/terminology/admin/value-set`,
+        valueSet,
+        {
+          headers: {
+            Authorization: `Bearer ${this.getAccessToken()}`,
+          },
+        }
+      );
+
+      return response.data;
+    } catch (error: any) {
+      const apiValidationError =
+        error.response?.data?.validationErrors?.["/api"];
+      if (apiValidationError) {
+        throw new Error(apiValidationError);
+      }
+
+      let message =
+        "An error occurred while updating the value set. Please try again. If the error persists, please contact the help desk.";
+
+      if (error.response?.data?.message) {
+        message = `${message}: ${error.response.data.message}`;
+      }
+
+      throw new Error(message);
+    }
+  }
+
+  async addValueSet(
+    valueSet: AddValueSetForAdmin
+  ): Promise<ValueSetDisplayForAdmin> {
+    try {
+      const response = await axios.post(
+        `${this.baseUrl}/terminology/admin/value-set`,
+        valueSet,
+        {
+          headers: {
+            Authorization: `Bearer ${this.getAccessToken()}`,
+          },
+        }
+      );
+
+      return response.data;
+    } catch (error: any) {
+      const apiValidationError =
+        error.response?.data?.validationErrors?.["/api"];
+      if (apiValidationError) {
+        throw new Error(apiValidationError);
+      }
+
+      let message =
+        "An error occurred while adding the value set. Please try again. If the error persists, please contact the help desk.";
+
+      if (error.response?.data?.message) {
+        message = `${message}: ${error.response.data.message}`;
+      }
+
+      throw new Error(message);
     }
   }
 
